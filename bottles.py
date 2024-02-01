ed003e5928aa2dc35aeea12977f45a98b21c5d55
David Gaither <gaither@protonmail.com>
2024-02-01 15:48:37 -0500

    bottles.py solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,23 @@
-def bottle_song():
-	# write your code here!
-	pass
+# make function sing_song that takes in an arg called num
+# make function second_part
+# make function for bottle/s
 
-bottle_song()
+# make variable called first_part equal to first paragraph
 
-####kldjsfalkfdajalskdfjdfls
+def bottle_song(num):
+    while num > 1:
+        print(f"""{num} {bottles(num)} of beer on the wall, {num} {bottles(num)} of beer.\nTake one down and pass it around, {num-1} {bottles(num)} of beer on the wall.""")
+        num -= 1
+    if num > 0:
+        print(f"""{num} {bottles(num)} of beer on the wall, {num} {bottles(num)} of beer.\nTake one down and pass it around, no more bottles of beer on the wall.""")
+        num -= 1
+    if num == 0:
+        return (f"""No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.""")
+    
+def bottles(num):
+    if num == 1:
+        return "bottle"
+    return "bottles"
+    
+
+print(bottle_song(99))
